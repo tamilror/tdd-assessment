1f2c12a23385e493d5289651ca90d561a50f7cc3
Tamil <tamilror@gmail.com>
2025-07-29 19:16:14 +0530

    changes commit

--- app/models/tdd-assessment.rb
+++ app/models/tdd-assessment.rb
@@ -1,31 +1,30 @@
-def add(numbers)
+class StringCalculator
+  def self.add(numbers)
     return 0 if numbers.strip.empty?
-  
+
     delimiter = /,|\n/
     if numbers.start_with?("//")
       header, numbers = numbers.split("\n", 2)
       custom_delimiter = header[2..]
       delimiter = Regexp.escape(custom_delimiter)
     end
-  
-    # Split and convert to integers
+
     nums = numbers.split(/#{delimiter}/).map(&:to_i)
-  
-    # Check for negative numbers
+
     negatives = nums.select { |n| n < 0 }
     unless negatives.empty?
       raise "negative numbers not allowed #{negatives.join(',')}"
     end
-  
+
     nums.sum
   end
+end
 
-#Test Result
-
-#puts add("")                # => 0
-#puts add("1")               # => 1
-#puts add("1,5")             # => 6
-#puts add("1\n2,3")          # => 6
-#puts add("//;\n1;2")        # => 3
-#puts add("//#\n2#3#4")      # => 9
-#puts add("4,-2,5,-7")       # => Exception: negative numbers not allowed -2,-7
+# Test cases
+puts StringCalculator.add("")                # => 0
+puts StringCalculator.add("1")               # => 1
+puts StringCalculator.add("1,5")             # => 6
+puts StringCalculator.add("1\n2,3")          # => 6
+puts StringCalculator.add("//;\n1;2")        # => 3
+puts StringCalculator.add("//#\n2#3#4")      # => 9
+puts StringCalculator.add("4,-2,5,-7")       # => Exception
